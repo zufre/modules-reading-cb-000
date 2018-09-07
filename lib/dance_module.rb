6424238f579f022161b2a7cdbee4f1e82a96d9aa
Cedric David <cedric.david@btopenworld.com>
2018-09-07 02:01:27 +0000

    automatically backed up by learn

--- lib/dance_module.rb
+++ lib/dance_module.rb
@@ -1,18 +1,27 @@
-module Dance
-
-  def twirl
-    "I'm twirling!"
+module FancyDance
+  module InstanceMethods
+ 
+    def twirl
+      "I'm twirling!"
+    end
+ 
+    def jump
+      "Look how high I'm jumping!"
+    end
+ 
+    def pirouette
+      "I'm doing a pirouette"
+    end
+ 
+    def take_a_bow
+      "Thank you, thank you. It was a pleasure to dance for you all."
+    end
   end
-
-  def jump
-    "Look how high I'm jumping!"
-  end
-
-  def pirouette
-    "I'm doing a pirouette"
-  end
-
-  def take_a_bow
-    "Thank you, thank you. It was a pleasure to dance for you all."
+ 
+  module ClassMethods
+ 
+    def metadata
+      "This class produces objects that love to dance."
+    end
   end
 end
