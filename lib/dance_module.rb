cfca3e4dc66a23c389c10cd4b63d2cdef192152a
Cedric David <cedric.david@btopenworld.com>
2018-09-07 01:51:59 +0000

    automatically backed up by learn

--- lib/dance_module.rb
+++ lib/dance_module.rb
@@ -2,15 +2,15 @@ module Dance
   def twirl
     "I'm twirling!"
   end
- 
+
   def jump
     "Look how high I'm jumping!"
   end
- 
+
   def pirouette
     "I'm doing a pirouette"
   end
- 
+
   def take_a_bow
     "Thank you, thank you. It was a pleasure to dance for you all."
   end
--- lib/kid.rb
+++ lib/kid.rb
@@ -1,3 +1,6 @@
 
 require_relative './dance_module.rb'
 require_relative './class_methods_module.rb'
+
+class Kid
+end
