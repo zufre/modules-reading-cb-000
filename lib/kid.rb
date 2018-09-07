1f7096cc41de0570ccf91b814512a40c812f6600
Cedric David <cedric.david@btopenworld.com>
2018-09-07 01:55:17 +0000

    automatically backed up by learn

--- lib/kid.rb
+++ lib/kid.rb
@@ -3,12 +3,11 @@ require_relative './dance_module.rb'
 require_relative './class_methods_module.rb'
 
 class Kid
-include Dance
+  include Dance
 
-attr_accessor :name
+  attr_accessor :name
 
-def initialize(name)
-  @name = name
+  def initialize(name)
+    @name = name
+  end
 end
-
-
